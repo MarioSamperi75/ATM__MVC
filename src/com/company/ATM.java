a4c821e6f3ca3c561e9114aa413935efd3762063
Mario Samperi <you@example.com>
2020-06-16 20:50:32 +0200

    Initial commit

---
+++ src/com/company/ATM.java
@@ -0,0 +1,153 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ATM {
+    int totalAvailable;
+    Bill thousand;
+    Bill fiveHundred;
+    Bill hundred;
+    List<Bill> bills;
+
+//--------------------------------------
+
+
+    public ATM() {
+        charging();
+        calculateTotalAmount();
+    }
+
+//--------------------------------------
+
+    public int getTotalAvailable() {
+        return totalAvailable;
+    }
+
+    public void setTotalAvailable(int totalAvailable) {
+        this.totalAvailable = totalAvailable;
+    }
+
+    public Bill getThousand() {
+        return thousand;
+    }
+
+    public void setThousand(Bill thousand) {
+        this.thousand = thousand;
+    }
+
+    public Bill getFiveHundred() {
+        return fiveHundred;
+    }
+
+    public void setFiveHundred(Bill fiveHundred) {
+        this.fiveHundred = fiveHundred;
+    }
+
+    public Bill getHundred() {
+        return hundred;
+    }
+
+    public void setHundred(Bill hundred) {
+        this.hundred = hundred;
+    }
+
+    public List<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(List<Bill> bills) {
+        this.bills = bills;
+    }
+
+
+//--------------------------------------
+
+    public void charging() {
+        this.thousand = new Bill("Thousand", 1000, 2);          //laddning det ska hända från en method
+        this.fiveHundred = new Bill("Five hundred", 500, 3);
+        this.hundred = new Bill("Hundred", 100, 5);
+        this.bills = new ArrayList<>();
+        bills.add(thousand);
+        bills.add(fiveHundred);
+        bills.add(hundred);
+    }
+
+    public void calculateTotalAmount(){
+        this.setTotalAvailable(0);
+        for (Bill bill: bills)
+        {this.totalAvailable += bill.getValue() * bill.getQuantity();}
+    }
+
+    public void withdraw(int withdrawal) {
+        Response response = checkAvailability(withdrawal);
+        if (response.getStatus())
+            eliminateBills(withdrawal);
+            //this.totalAvailable = totalAvailable - withdrawal;
+    }
+
+    public Response checkAvailability(int withdrawal){
+        if (withdrawal> totalAvailable)
+            return new Response("amount not available", false);
+        if ((withdrawal % 100) != 0)
+            return new Response("amount not payable", false);
+
+        int withdrawalHundreds = getLastThreeDigits(withdrawal);
+        int ATMTotalHundred = hundred.getValue() * hundred.getQuantity();
+        int ATMTotalFiveHundreds =fiveHundred.getValue() * fiveHundred.getQuantity();
+
+        if (withdrawalHundreds<500 && ATMTotalHundred < withdrawalHundreds)
+                return new Response("amount not payable", false);
+
+        if (withdrawalHundreds>=500)
+            if  (ATMTotalFiveHundreds == 0 && ATMTotalHundred <  withdrawalHundreds      ||
+                 ATMTotalFiveHundreds  > 0 && ATMTotalHundred < (withdrawalHundreds - 500))
+                    return new Response("amount not payable", false);
+
+        return new Response("amount available", true);
+    }
+
+    public void eliminateBills(int withdrawal){
+        int left = withdrawal;
+
+        while (left>=1000) {
+            if (thousand.getQuantity() > 0) {
+                thousand.setQuantity(thousand.getQuantity() - 1);
+                left -= 1000;
+            } else if (fiveHundred.getQuantity() > 0) {
+                fiveHundred.setQuantity(fiveHundred.getQuantity() - 1);
+                left -= 500;
+            } else if (hundred.getQuantity() > 0) {
+                hundred.setQuantity(hundred.getQuantity() - 1);
+                left -= 100;
+            }
+        }
+
+        while (left>=500) {
+            if (fiveHundred.getQuantity() > 0) {
+                fiveHundred.setQuantity(fiveHundred.getQuantity() - 1);
+                left -= 500;
+            } else if (hundred.getQuantity() > 0) {
+                hundred.setQuantity(hundred.getQuantity() - 1);
+                left -= 100;
+            }
+        }
+
+        while (left>=100) {
+            if (hundred.getQuantity() > 0) {
+                hundred.setQuantity(hundred.getQuantity() - 1);
+                left -= 100;
+            }
+        }
+        calculateTotalAmount();
+    }
+
+
+    public int getLastThreeDigits(int withdrawal) { // att fixa
+        String withdrawalAsString =  Integer.toString(withdrawal);
+        String lastTreeDigitsAsString = withdrawalAsString.substring(withdrawalAsString.length() - 3);
+        int lastTreeDigitsAsInt = Integer.parseInt(lastTreeDigitsAsString);
+        return lastTreeDigitsAsInt;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,90 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+
+        while (true) {
+
+            int totalBills = 0;
+            for (Bill bill : atm.getBills()) {
+                totalBills += bill.getQuantity();
+            }
+            System.out.println("Totali banconote: " + totalBills);
+
+            for (Bill bill : atm.getBills()) {
+                System.out.println(bill.getName() + ": " + bill.getQuantity());
+            }
+
+            int totalAmount = 0;
+            for (Bill bill : atm.getBills()) {
+                totalAmount += bill.getValue() * bill.getQuantity();
+            }
+            System.out.println("Total Amount: " + totalAmount);
+            boolean checkInput = false;
+            int withdrawal = 0;
+            while (checkInput == false) {
+                checkInput = true;
+                String input = JOptionPane.showInputDialog("withdrawal?");
+
+                if (input == null){
+                    System.out.println("bye bye");
+                    System.exit(0);
+                }
+                try {
+                    withdrawal = Integer.parseInt(input);
+                } catch (NumberFormatException e) {
+                    System.out.println("Input not allowed");
+                    checkInput = false;
+                }
+            }
+            Response checkAvalaibility = atm.checkAvailability(withdrawal);
+            if (!checkAvalaibility.getStatus()) {
+                System.out.println(checkAvalaibility.getMessage());
+                System.out.println();
+            }
+            else {
+                atm.withdraw(withdrawal);
+                System.out.println("New Total Amount: " + atm.getTotalAvailable());
+                System.out.println();
+            }
+
+            int exitCheck = JOptionPane.showConfirmDialog(null, "Would you like to continue?");
+            if (exitCheck!=0) {
+                System.out.println("bye bye");
+                System.exit(0);
+            }
+
+
+
+        }
+    }
+}
+
+/* Description
+Your assignment is to build an ATM in your programming language of choice.
+The ATM should operate under the given rules:
+● The bills used for withdrawal is removed from the ATM
+● The ATM should always return the smallest amount of bills possible
+○ Example: a withdrawal of 500 should return 1 x 500 bill rather than 5 x 100 bills if possible
+● The ATM can't give out more money than it contains
+● The ATM can't give out amounts that the remaining bills don’t add up to
+________________________________________
+The assignment
+The ATM starts with a predetermined amount of each bill:
+● 2 x 1000
+● 3 x 500
+● 5 x 100
+You will make seven withdrawals the specified order. Note that based on the rules listed earlier some of the withdrawals should be declined by the ATM:
+1.	1500
+2.	 700
+3.	400
+4.	1100
+5.	1000
+6.	700
+7.	300
+When all of these withdrawals are completed the ATM is expected to be completely empty of bills and will not have given out more money than it contained at the start.
+*/
